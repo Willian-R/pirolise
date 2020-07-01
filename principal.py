97e8cd6c75751c9137c83eae4556e12c699d91f8
Willian Ribeiro <64652475+Willian-R@users.noreply.github.com>
2020-07-01 14:57:14 -0300

    script principal

---
+++ principal.py
@@ -0,0 +1,5 @@
+import redeneural
+redeneural.RedeNeural.treinamento()
+'''pirolise = [1, 0, 0.74, 0, 1, 0, 0.44, 1, 1]
+resposta = redeneural.RedeNeural.simulacao(pirolise, w1, w2, b1, b2)
+print(resposta)'''
